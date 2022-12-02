14b6155268620c7cf4a59c8755dfe75c8893ee69
jtr354 <jtr354@foxmail.com>
2022-12-02 09:01:47 +0800

    feat: api

--- src/application/Recommend/index.jsx
+++ src/application/Recommend/index.jsx
@@ -1,38 +1,74 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+
+import Scroll from '../../baseUI/scroll';
+
 import Slider from '../../components/slider';
 import RecommendList from '../../components/list';
-import Scroll from '../../components/scroll';
+
+import * as actionTypes from './store/actionCreators';
 import { Content } from './style';
 
-function Recommend() {
-	//mock 数据
-	const bannerList = [1, 2, 3, 4].map((item) => {
-		return {
-			imageUrl:
-				'http://p1.music.126.net/ZYLJ2oZn74yUz5x8NBGkVA==/109951164331219056.jpg'
-		};
-	});
+function Recommend(props) {
+	const { bannerList, recommendList } = useSelector((state) => ({
+		// 不要在这里将数据 toJS
+		// 不然每次 diff 比对 props 的时候都是不一样的引用，还是导致不必要的重渲染，属于滥用 immutable
+		bannerList: state.getIn(['recommend', 'bannerList']),
+		recommendList: state.getIn(['recommend', 'recommendList'])
+	}));
 
-	const recommendList = new Array(20).fill(0).map((item) => {
-		return {
-			id: 1,
-			picUrl:
-				'https://p1.music.126.net/fhmefjUfMD-8qtj3JKeHbA==/18999560928537533.jpg',
-			playCount: 17171122,
-			name: '朴树、许巍、李健、郑钧、老狼、赵雷'
-		};
-	});
+	const dispatch = useDispatch();
+
+	useEffect(() => {
+		function getBannerDataDispatch() {
+			dispatch(actionTypes.getBannerList());
+		}
+		function getRecommendListDataDispatch() {
+			dispatch(actionTypes.getRecommendList());
+		}
+		getBannerDataDispatch();
+		getRecommendListDataDispatch();
+		//eslint-disable-next-line
+	}, []);
+
+	const bannerListJS = bannerList ? bannerList.toJS() : [];
+	const recommendListJS = recommendList ? recommendList.toJS() : [];
 
 	return (
 		<Content>
-			<Scroll className="list">
+			<Scroll>
 				<div>
-					<Slider bannerList={bannerList}></Slider>
-					<RecommendList recommendList={recommendList}></RecommendList>
+					<Slider bannerList={bannerListJS}></Slider>
+					<RecommendList recommendList={recommendListJS}></RecommendList>
 				</div>
 			</Scroll>
 		</Content>
 	);
 }
+
+// // 映射 Redux 全局的 state 到组件的 props 上
+// const mapStateToProps = (state) => ({
+// 	// 不要在这里将数据 toJS
+// 	// 不然每次 diff 比对 props 的时候都是不一样的引用，还是导致不必要的重渲染，属于滥用 immutable
+// 	bannerList: state.getIn(['recommend', 'bannerList']),
+// 	recommendList: state.getIn(['recommend', 'recommendList'])
+// });
+// // 映射 dispatch 到 props 上
+// const mapDispatchToProps = (dispatch) => {
+// 	return {
+// 		getBannerDataDispatch() {
+// 			dispatch(actionTypes.getBannerList());
+// 		},
+// 		getRecommendListDataDispatch() {
+// 			dispatch(actionTypes.getRecommendList());
+// 		}
+// 	};
+// };
+
+// 将 ui 组件包装成容器组件
+// export default connect(
+// 	mapStateToProps,
+// 	mapDispatchToProps
+// )(React.memo(Recommend));
 
 export default React.memo(Recommend);
--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,6 +1,7 @@
-//reducer.js
 import { combineReducers } from 'redux-immutable';
+import { reducer as recommendReducer } from '../application/Recommend/store/index';
 
 export default combineReducers({
-	// 之后开发具体功能模块的时候添加 reducer
+	// 之后开发具体功能模块的时候添加reducer
+	recommend: recommendReducer
 });
