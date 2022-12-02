1ec4e14e10afa901665609413187b4a327c9e72d
jtr354 <jtr354@foxmail.com>
2022-12-02 11:06:02 +0800

    feat: debugger for devices

---
+++ src/debugger.js
@@ -0,0 +1,11 @@
+import VConsole from 'vconsole';
+
+new VConsole();
+// or init with options
+// const vConsole = new VConsole({ theme: 'dark' });
+
+// call `console` methods as usual
+// console.log('Hello world');
+
+// remove it when you finish debugging
+// vConsole.destroy();
