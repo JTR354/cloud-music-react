569e87307ffba65e5f0925686886e5dec99cfb0f
jtr354 <jtr354@foxmail.com>
2023-09-22 23:30:19 +0800

    feat: reset project

--- src/api/request.js
+++
@@ -1,9 +0,0 @@
-import { axiosInstance } from './config';
-
-export const getBannerRequest = () => {
-	return axiosInstance.get('/banner');
-};
-
-export const getRecommendListRequest = () => {
-	return axiosInstance.get('/personalized');
-};
