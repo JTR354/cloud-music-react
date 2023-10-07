9fe79fabaef523791ab00c62c08991b67ca70453
jtr354 <jtr354@foxmail.com>
2023-10-08 00:15:13 +0800

    feat: vconsole

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import VConsole from 'vconsole';
 
 import App from './App.tsx';
 
+new VConsole();
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
